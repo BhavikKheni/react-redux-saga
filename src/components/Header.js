8e7c8a9cc318dd2edafe7538109fd6ce8e036028
BhavikKheni <kheni.bhavik@gmail.com>
2023-01-12 19:15:04 +0530

    refactor code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,34 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom';
-import { productSearch } from '../redux/productAction'
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { productSearch } from "../redux/productAction";
 const Header = () => {
-    const result = useSelector((state) => state.cartData);
-    const dispatch = useDispatch();
-    return (
-        <div className="header">
-            <Link to="/"><h1 className='logo'>E-Comm</h1></Link>
-            <div className='search-box'>
-                <input type="text" onChange={(event) => dispatch(productSearch(event.target.value))} placeholder='Search Product' />
-            </div>
-            <Link to="/cart">
-                <div className="cart-div">
-                    <span>{result.length}</span>
-                    <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt="" />
-                </div></Link>
+  const result = useSelector((state) => state.cartData);
+  const dispatch = useDispatch();
+  return (
+    <div className="header">
+     
+        <div className="company-name">
+          <h1>Buy</h1>
+            <input
+              type="text"
+              onChange={(event) => dispatch(productSearch(event.target.value))}
+              placeholder="Search Product"
+              className="search-box"
+            />
         </div>
-    )
-}
+      <div>
+        <Link to="/cart">
+          <div className="cart-div">
+            <span>{result.length}</span>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/263/263142.png"
+              alt=""
+            />
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
